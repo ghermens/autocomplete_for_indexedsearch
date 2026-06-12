3c2ce27a670a9762c332b78d3936f5e8938d487c
Gregor Hermens <gregor.hermens@a-mazing.de>
2026-06-12 13:39:39 +0200

    [FEATURE] Improve accessibility with aria and role attributes
    (cherry picked from commit e60120a33b40a6186aeee0fd72746f55cf25b5f3)

--- Classes/Controller/AutocompleteController.php
+++ Classes/Controller/AutocompleteController.php
@@ -42,6 +42,8 @@ final class AutocompleteController extends ActionController
 		$caretpos = $this->request->hasArgument('caretpos') ? $this->request->getArgument('caretpos') : 0;
 		$caretpos = is_numeric($caretpos) ? (int) $caretpos : strlen($input);
 
+		$listboxid = $this->request->hasArgument('listboxid') ? $this->request->getArgument('listboxid') : '';
+
 		$words = explode(' ', $input);
 		$wordKey = SearchWordsArrayUtility::getCurrentWordKey($words, $caretpos);
 		if ($words[$wordKey] !== '') {
@@ -64,6 +66,7 @@ final class AutocompleteController extends ActionController
 		$suggestions = $event->getSuggestions();
 
 		$this->view->assign('suggestions', $suggestions);
+		$this->view->assign('listboxid', $listboxid);
 
 		return $this->htmlResponse();
 	}
